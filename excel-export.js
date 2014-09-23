23198d33c46de99a42a894ba8cee750325e08237
Eriks Klotins <eriks.klotins@gmail.com>
2014-09-23 10:49:34 +0200

    Now links to results are exported

--- excel-export.js
+++ excel-export.js
@@ -11,52 +11,53 @@
 
 ;(function($, window)
 {
-    var results = [];
+  var results = [];
 	var installButton = function(title, action)
-    {
-       var node = $('<a href="#">'+title+'</a>'); 
-       return $(node)
-       	.on('click', action)
-        .appendTo('#gs_gb_lt');
-    }
+  {
+     var node = $('<a href="#">'+title+'</a>'); 
+     return $(node)
+     	.on('click', action)
+      .appendTo('#gs_gb_lt');
+  };
     
-    var getQueryInfo = function()
-    {
-       var offset = window.location.href.match(/start=([0-9]+)/);
-       offset = (offset) ? parseInt(offset[1]): 0; 
-        
-       var num = window.location.href.match(/num=([0-9]+)/);
-       num = (num) ? parseInt(offset[1]): 0;  
-        
-       var q = window.location.href.match(/q=([^&]+)/);
-        
-        return {offset:offset, num:num, q:q[1]}
-    }
-    var parseResult = function(i, item)
-    {
-       var offset =  getQueryInfo().offset;
-       var index = offset+ i + 1; 
-       var title = $('.gs_rt',item).text().replace(/(\r\n|\n|\r)/gm,"");
-       var meta = $('.gs_a', item).text().replace(/(\r\n|\n|\r)/gm,"");
-       var abstract = $('.gs_rs', item).text().replace(/(\r\n|\n|\r)/gm,"");
-        results.push({ index:index,title: title, meta: meta, abstract:abstract});
-       //console.log([title,authors,abstract]);
-    }
+  var getQueryInfo = function()
+  {
+     var offset = window.location.href.match(/start=([0-9]+)/);
+     offset = (offset) ? parseInt(offset[1]): 0; 
+      
+     var num = window.location.href.match(/num=([0-9]+)/);
+     num = (num) ? parseInt(offset[1]): 0;  
+      
+     var q = window.location.href.match(/q=([^&]+)/);
+      
+      return {offset:offset, num:num, q:q[1]};
+  };
+
+  var parseResult = function(i, item)
+  {
+    var offset =  getQueryInfo().offset;
+    var index = offset+ i + 1; 
+    var title = $('.gs_rt',item).text().replace(/(\r\n|\n|\r)/gm,"");
+    var meta = $('.gs_a', item).text().replace(/(\r\n|\n|\r)/gm,"");
+    var abstract = $('.gs_rs', item).text().replace(/(\r\n|\n|\r)/gm,"");
+    results.push({ index:index,title: title, meta: meta, abstract:abstract});
+     //console.log([title,authors,abstract]);
+  };
     
     var encodeCSV = function (results)
     {
         var result = '';
     	for(var i=0;i<results.length;i++)
+      {
+        var row = [];
+  			for(var col in results[i])
         {
-            var row = [];
-			for(var col in results[i])
-            {
-                row.push(results[i][col]);
-            }
-            result += row.join("\t")+"\n";
+            row.push(results[i][col]);
         }
-       return result;
-    }
+        result += row.join("\t")+"\n";
+      }
+      return result;
+    };
     
     var parsePage = function()
     {
@@ -67,7 +68,7 @@
        displayResults(str);
      //   console.log(results.length, results);
         
-    }
+    };
     
     
     var displayResults = function(results)
@@ -78,8 +79,16 @@
         $('textarea',node).select();
         $('button[data-action="next"]',node).on('click', function(){ 
         	var url = $('b.gs_nma + a').attr('href');
-            //console.log(url);
+          if (!url)
+          {
+            alert('You have reached the end!');
+          }
+          else
+          {
             document.location = url;
+          }
+            //console.log(url);
+           
         });
         
          $('button[data-action="close"]',node).on('click', function(){ 
@@ -87,7 +96,7 @@
              
              $('.ps_results').remove();
         });
-    }
+    };
   
     
     var button1 = installButton('Export', parsePage);
